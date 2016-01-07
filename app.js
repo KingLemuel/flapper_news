d16b06a3d1a06f622e9fa03814bc587c1e370365
kinglemuel <pro@kinglemuel.com>
2016-01-06 21:00:58 -0500

    add home template to index

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
-angular.module('flapperNews', [])
+angular.module('flapperNews', ['ui.router'])
+.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+	$stateProvider
+		.state('home', {
+			url: '/home',
+			templateUrl: '/home.html',
+			controller: 'MainCtrl'
+		});
+		
+		$urlRouterProvider.otherwise('home');
+}])
+
+.factory('posts', [function(){
+	var o = {
+		posts: []
+	};
+	return o;
+}])
+
 .controller('MainCtrl', [
 '$scope',
-function($scope){
-	$scope.test = "Hello world!";
-	$scope.posts = [
-		{title: 'post 1', upvotes: 5},
-		{title: 'post 2', upvotes: 2},
-		{title: 'post 3', upvotes: 15},
-		{title: 'post 4', upvotes: 9},
-		{title: 'post 5', upvotes: 4}
-	];
+'posts',
+function($scope, posts){
+	$scope.posts = posts.posts;
 	$scope.addPost = function() {
 		if(!$scope.title || $scope.title === '') { return; }
 		$scope.posts.push({ 
@@ -26,3 +41,10 @@ function($scope){
 	};
 }]);
 
+	// $scope.posts = [
+	// 	{title: 'post 1', upvotes: 5},
+	// 	{title: 'post 2', upvotes: 2},
+	// 	{title: 'post 3', upvotes: 15},
+	// 	{title: 'post 4', upvotes: 9},
+	// 	{title: 'post 5', upvotes: 4}
+	// ];
